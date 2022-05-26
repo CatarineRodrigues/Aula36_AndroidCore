4a59d5efa52e734692b3504a96271f26ba73da3c
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-05-26 12:36:28 -0300

    Correcao na exibicao dos detalhes do filme

--- ListaExercicios/Filme/app/src/main/java/br/com/zup/filme/DetalheFilme.kt
+++ ListaExercicios/Filme/app/src/main/java/br/com/zup/filme/DetalheFilme.kt
@@ -1,10 +1,7 @@
 package br.com.zup.filme
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 
 class DetalheFilme : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
