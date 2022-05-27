44cfccf4d95be423a8e73fa63d5cf9afdb5792fa
CatarineRodrigues <catarine_pacheco@hotmail.com>
2022-05-26 21:35:36 -0300

    Implementacao do exercicio 3 minha musica favorita

--- ListaExercicios/Filme/app/src/main/java/br/com/zup/filme/DetalheFilme.kt
+++ ListaExercicios/Filme/app/src/main/java/br/com/zup/filme/DetalheFilme.kt
@@ -2,10 +2,42 @@ package br.com.zup.filme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class DetalheFilme : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(MainActivity.TAG, "Passou pelo onCreate() do detalhe")
         setContentView(R.layout.activity_detalhe_filme)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(MainActivity.TAG, "Passou pelo onStart() do detalhe")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(MainActivity.TAG, "Passou pelo onResune() do detalhe")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(MainActivity.TAG, "Passou pelo onPause() do detalhe")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(MainActivity.TAG, "Passou pelo onStop() do detalhe")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(MainActivity.TAG, "Passou pelo onDestroy() do detalhe")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(MainActivity.TAG, "Passou pelo onRestart() do detalhe")
     }
 }
--- ListaExercicios/Filme/app/src/main/java/br/com/zup/filme/MainActivity.kt
+++ ListaExercicios/Filme/app/src/main/java/br/com/zup/filme/MainActivity.kt
@@ -3,18 +3,25 @@ package br.com.zup.filme
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG = "APP_FILME"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "Passou pelo onCreate() da main")
         setContentView(R.layout.activity_main)
 
         val botaoMensagem = findViewById<Button>(R.id.botao_mensagem)
 
         botaoMensagem.setOnClickListener {
-            Toast.makeText(this,"Quero muito ver esse filme", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "Quero muito ver esse filme", Toast.LENGTH_LONG).show()
         }
 
         val botaoDetalheFilme = findViewById<Button>(R.id.botao_detalhe_filme)
@@ -23,5 +30,38 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, DetalheFilme::class.java)
             startActivity(intent)
         }
+
+        val botaoEmail = findViewById<Button>(R.id.botao_email)
+
+        botaoEmail.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.type = "text/html"
+            intent.putExtra(Intent.EXTRA_EMAIL, "emaildo@diretor.com")
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Assunto")
+            intent.putExtra(Intent.EXTRA_TEXT, "Eu sou o corpo do email")
+
+            startActivity(intent)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "Passou pelo onStart() da main")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "Passou pelo onResune() da main")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "Passou pelo onPause() da main")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "Passou pelo onStop() da main")
     }
 }
